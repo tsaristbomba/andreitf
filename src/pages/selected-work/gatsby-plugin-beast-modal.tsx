cc78dcdb89ec5a80319bed0633d6907a684b0ed8
vorsakha <andreitf.dev@gmail.com>
2021-08-16 09:50:54 -0300

    selected-work - beast-modal

--- src/pages/selected-work/gatsby-plugin-beast-modal.tsx
+++ src/pages/selected-work/gatsby-plugin-beast-modal.tsx
@@ -1,11 +1,12 @@
 import { navigate } from "gatsby"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 // Components
 import Layout from "../../components/layout"
 import SelectedWork from "../../components/PagesComponents/SelectedWork"
 import Seo from "../../components/seo"
 import Loading from "../../components/common/loading"
+import { useIntl } from "gatsby-plugin-intl"
 
 // Types
 type WorkPageTypes = {
@@ -25,15 +26,47 @@ type WorkPageTypes = {
 }
 
 const WorkPage: React.FC<WorkPageTypes> = ({ location }): JSX.Element => {
+  const [data, setData] = useState(null)
+
   useEffect(() => {
     location.state === null && navigate("/")
+  }, [])
+
+  const intl = useIntl()
+
+  useEffect(() => {
+    const title = intl.formatMessage({ id: "page-5-title" })
+    const page = intl.formatMessage({ id: "page-5-page" })
+    const npm = intl.formatMessage({ id: "page-5-npm" })
+    const repo = intl.formatMessage({ id: "page-5-repo" })
+    const description = intl.formatMessage({ id: "page-5-description" })
+    const image = intl.formatMessage({ id: "page-5-image" })
+    const challenges = "page-5-challenges"
+    const stack = [
+      "Gatsby.js",
+      "Typescript",
+      "styled-components",
+      "gatsby-plugin",
+      "GraphQL",
+    ]
+
+    setData({
+      title,
+      page,
+      npm,
+      repo,
+      description,
+      image,
+      challenges,
+      stack,
+    })
   }, [])
 
   return (
     <Layout>
       <Seo title={location.state === null ? "" : location.state.title} />
-      {location.state !== null ? (
-        <SelectedWork {...location.state} />
+      {data !== null ? (
+        <SelectedWork {...data} />
       ) : (
         <div
           style={{ minHeight: "calc(100vh - 120px)", background: "#2D2D2D" }}
