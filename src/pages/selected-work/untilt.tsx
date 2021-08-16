d113191fadcb63080a545305dd3b2294199f92f1
vorsakha <andreitf.dev@gmail.com>
2021-08-16 09:51:29 -0300

    selected-work - untilt

--- src/pages/selected-work/untilt.tsx
+++ src/pages/selected-work/untilt.tsx
@@ -1,11 +1,12 @@
 import { navigate } from "gatsby"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 // Components
 import Layout from "../../components/layout"
 import SelectedWork from "../../components/PagesComponents/SelectedWork"
 import Seo from "../../components/seo"
 import Loading from "../../components/common/loading"
+import { useIntl } from "gatsby-plugin-intl"
 
 // Types
 type WorkPageTypes = {
@@ -25,15 +26,49 @@ type WorkPageTypes = {
 }
 
 const WorkPage: React.FC<WorkPageTypes> = ({ location }): JSX.Element => {
+  const [data, setData] = useState(null)
+
   useEffect(() => {
     location.state === null && navigate("/")
+  }, [])
+
+  const intl = useIntl()
+
+  useEffect(() => {
+    const title = intl.formatMessage({ id: "page-4-title" })
+    const page = intl.formatMessage({ id: "page-4-page" })
+    const link = intl.formatMessage({ id: "page-4-link" })
+    const repo = intl.formatMessage({ id: "page-4-repo" })
+    const description = intl.formatMessage({ id: "page-4-description" })
+    const image = intl.formatMessage({ id: "page-4-image" })
+    const challenges = "page-4-challenges"
+    const stack = [
+      "React.js",
+      "Typescript",
+      "Tailwindcss",
+      "Redux",
+      "Express",
+      "Axios",
+      "MongoDB",
+    ]
+
+    setData({
+      title,
+      page,
+      link,
+      repo,
+      description,
+      image,
+      challenges,
+      stack,
+    })
   }, [])
 
   return (
     <Layout>
       <Seo title={location.state === null ? "" : location.state.title} />
-      {location.state !== null ? (
-        <SelectedWork {...location.state} />
+      {data !== null ? (
+        <SelectedWork {...data} />
       ) : (
         <div
           style={{ minHeight: "calc(100vh - 120px)", background: "#2D2D2D" }}
